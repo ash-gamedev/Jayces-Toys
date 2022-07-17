929176b2df89e347d4203d3c7481614e77fd3185
ash-gamedev <adrouill1997@gmail.com>
2022-07-17 00:09:01 -0400

    Updates pt 2

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void LoadLevelSelectorScene()
+    {
+        SceneManager.LoadScene("LevelSelection");
+    }
+
+    public void LoadPotatoeHeadScene()
+    {
+        SceneManager.LoadScene("PotatoeHead");
+    }
+}
