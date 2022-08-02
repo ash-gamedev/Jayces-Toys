43ddd7b46b64e82c5e20488aad0c5ccbf7ae6e6e
ash-gamedev <adrouill1997@gmail.com>
2022-08-01 20:26:10 -0400

    WIP - Etch a sketch

---
+++ Assets/Scripts/Games/EtchSketchGame.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EtchSketchGame : Game
+{
+    [SerializeField] private List<Transform> shapeTransforms; 
+    [SerializeField] private LineController dottedLine;
+    [SerializeField] private LineController line;
+
+    private List<Transform> currentShape;
+    System.Random random;
+
+    #region Game states
+    public override void OnPrepareGame()
+    {
+        base.OnPrepareGame();
+
+        random = new System.Random();
+
+        OnPrepareLevel();
+    }
+
+    public override void OnPrepareLevel()
+    {
+        base.OnPrepareLevel();
+
+        // select shape
+        SelectNextShape();
+
+        // play level
+        OnPlayLevel();
+    }
+
+    public override void OnPlayLevel()
+    {
+        base.OnPlayLevel();
+    }
+
+    public override bool IsLevelComplete()
+    {
+        return false;
+    }
+    #endregion
+
+    public void SelectNextShape()
+    {
+        // get random shape
+        int index = random.Next(shapeTransforms.Count);
+        currentShape = shapeTransforms[index].GetComponentsInChildren<Transform>().ToList();
+
+        // remove first point (center/parent)
+        currentShape.RemoveAt(0);
+
+        // add start point to end (to complete the shapes)
+        currentShape.Add(currentShape[0]);
+
+        // remove from list (so it doesn't get selected again)
+        shapeTransforms.RemoveAt(index);
+
+        // instantiate letter blocks for word
+        InstantiateDottedLine();
+    }
+
+    public void InstantiateDottedLine()
+    {
+        dottedLine.SetUpLine(currentShape);
+    }
+}
---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+
+    private LineRenderer lr;
+    private Transform[] points;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    public void SetUpLine(List<Transform> points)
+    {
+        lr.positionCount = points.Count;
+        this.points = points.ToArray();
+
+        foreach(Transform point in points)
+            Debug.Log(point.position);
+    }
+
+    private void Update()
+    {
+        if(points != null)
+        {
+            for (int i = 0; i < points?.Length; i++)
+            {
+                lr.SetPosition(i, points[i].position);
+            }
+        }
+    }
+}
