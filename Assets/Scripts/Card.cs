50579d00dff68a4cd8987f583150b5d99d0d5fff
ash-gamedev <adrouill1997@gmail.com>
2022-08-11 22:01:22 -0400

    Updated sound effects

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -78,7 +78,7 @@ public class Card : MonoBehaviour
         // scale down
         mySequence.Append(transform.DOScale(Vector3.zero, 0.75f));
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1f);
 
         isHidden = true;
     }
