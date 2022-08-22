5f28ff9da2027d57368361ef6ec82c159751705c
ash-gamedev <adrouill1997@gmail.com>
2022-08-21 23:55:18 -0400

    Add balloons to level complete scene

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Balloon : MonoBehaviour
+{
+    [SerializeField] private List<Color> colors;
+    [SerializeField] private float balloonSpeed = 20f;
+
+    Image image;
+    BalloonGenerator bl;
+
+    bool isTouched = false;
+
+    System.Random random;
+
+    void Start()
+    {
+        bl = FindObjectOfType<BalloonGenerator>();
+        image = GetComponent<Image>();
+        // get random color
+        random = new System.Random();
+        int index = random.Next(colors.Count);
+        Color color = colors[index];
+        image.color = color;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y + (balloonSpeed * Time.deltaTime));    
+    }
+
+    public void OnTouch()
+    {
+        if (isTouched) return;
+        isTouched = true;
+
+        // pick random pop sound to play
+        int index = random.Next(bl.balloonPopSounds.Count);
+        AudioSource.PlayClipAtPoint(bl.balloonPopSounds[index], Camera.main.transform.position, 0.5f);
+
+        StartCoroutine(ShowAnim());
+        Destroy(gameObject, 0.8f);
+    }
+
+    IEnumerator ShowAnim()
+    {
+        //image.sprite = bl.destrSp[spriteNumber];
+        //spriteNumber++;
+        //if (spriteNumber == bl.destrSp.Length)
+        //{
+        //    image.enabled = false;
+        //    yield break;
+        //}
+        yield return new WaitForSeconds(0.1f);
+        image.sprite = bl.destrSp[0];
+        //StartCoroutine(ShowAnim());
+        //yield return null;
+    }
+}
---
+++ Assets/Scripts/BalloonGenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BalloonGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject balloonPrefab;
+
+    [SerializeField] public List<AudioClip> balloonPopSounds;
+
+    public Sprite[] destrSp;
+
+    float spawnDelay = 0.3f;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        SpawnBalloon();
+        yield return new WaitForSeconds(spawnDelay);
+        StartCoroutine(Spawn());
+        yield return null;
+    }
+
+    void SpawnBalloon()
+    {
+        Vector2 spawnPosition = new Vector2(Screen.width * Random.Range(0.1f, 0.9f), -200f);
+        GameObject obj = Instantiate(balloonPrefab, spawnPosition, Quaternion.identity, transform);
+        obj.transform.localScale = Vector3.one;
+        Destroy(obj, 5f);
+        spawnDelay *= 0.999f;
+    }
+}
